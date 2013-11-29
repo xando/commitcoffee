41abc7f5bd3a7195a846f8677d85ba23c5dcdc31
robahtou <robotoes@hush.com>
2013-11-28 21:06:12 -0500

    New awesome coffee place

--- places.js
+++ places.js
@@ -686,5 +686,20 @@ var places = [
 			"Provision": "Coffee, tea, juices, cake, small breakfast foods",
 		},
 		"link": {"homepage":"https://plus.google.com/107552475726057576910/about"}
-	}
+	},
+	{
+                "city": "Boston",
+                "country": "USA",
+                "name": "Boston Common Coffe Co.",
+                "address": "515 Washington Street",
+                "coordinates": ["42.354217", "-71.061917"],
+                "description": {
+                        "Internet": "Free wifi with purchase. Ask for password.",
+                        "Power Outlets": "Seating near the walls",
+                        "Seating": "Chairs, high chairs, couches, coffe tables",
+                        "Service": "Alright, get's the job done. Nothing special.",
+                        "Provision": "coffee, tea, pastries, sandwiches and other foodies."
+                },
+                "link": {"homepage":"http://bostoncommoncoffee.com/locations-hours/boston-common-coffee-company-downtown-crossing-515-washington-street/"}
+        }
 ]
