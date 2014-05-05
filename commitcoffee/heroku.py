50472912766a8896295d90bea0dd3224dfb1022f
xando <sebastian.pawlus@gmail.com>
2014-05-05 16:32:00 +0200

    fixing

--- commitcoffee/heroku.py
+++ commitcoffee/heroku.py
@@ -3,7 +3,5 @@ from settings import *
 import dj_database_url
 
 DEBUG = False
-DATABASES = {
-    'default': dj_database_url.config()
-}
+DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
