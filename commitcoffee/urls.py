73a87a2bd860e84feb9b7ea8229a85090112873c
xando <sebastian.pawlus@gmail.com>
2014-05-05 14:32:14 +0200

    template view

--- commitcoffee/urls.py
+++ commitcoffee/urls.py
@@ -1,6 +1,8 @@
+import os
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
+from django.http import HttpResponse, CompatibleStreamingHttpResponse
 from django.contrib.staticfiles.views import serve
 
 from rest_framework import routers
@@ -11,11 +13,16 @@ router = routers.DefaultRouter()
 router.register(r'place', api.Place)
 
 
+def template(request, path):
+    path = path or 'index.html'
+    fullpath = os.path.join(settings.BASE_DIR, 'templates', path)
+    return CompatibleStreamingHttpResponse(open(fullpath, 'rb'), "text/html")
+
+
 urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += patterns(
     '',
     url(r'^api/search', api.ListUsers.as_view()),
     url(r'^api/', include(router.urls)),
-    url(r'', lambda request: serve(request, 'index.html')),
+    url(r'(^(?P<path>[^/]+.html)$)|(^$)', template),
 )
-
