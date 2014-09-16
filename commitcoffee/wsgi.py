0760bca59b313885d7e35976707c0dfb5dc3e4e1
xando <sebastian.pawlus@gmail.com>
2014-09-16 22:12:50 +0200

    fix wsgi

--- commitcoffee/wsgi.py
+++ commitcoffee/wsgi.py
@@ -10,9 +10,5 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "commitcoffee.settings")
 
-
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
-
-application = Cling(get_wsgi_application())
-
+application = get_wsgi_application()
