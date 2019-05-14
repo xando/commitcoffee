949b325fa317c02e79aeb2c265b14d169695b134
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2019-05-14 08:59:40 +0200

    Copy CNAME file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
 
 
 module.exports = {
@@ -41,6 +42,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/index.html",
             filename: "./index.html"
-        })
+        }),
+        new CopyPlugin([{ from: 'CNAME', to: '.' }])
     ]
 };
