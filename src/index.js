f1725fb26c41e92243d6170e5c1ab0697c59cbeb
Sebastian Pawluś <sebastian.pawlus@gmail.com>
2019-05-14 19:47:11 +0200

    chagne hight

--- src/index.js
+++ src/index.js
@@ -148,7 +148,7 @@ class App extends React.Component {
                     <SearchBox onChange={this.searchChanged} />
                 </Box>
 
-                <Box height="300px" ref={el => this.mapContainer = el} />
+                <Box height="420px" ref={el => this.mapContainer = el} />
 
                 {this.state.popup && (
                     <Layer
